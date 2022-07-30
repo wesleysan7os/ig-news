71b64a4ca77e60a07ac8ff06e2a42a202c5731ec
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-07-30 12:35:15 -0300

    feat: Homepage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head'
-import { Header } from '../components/Header'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contenteContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
